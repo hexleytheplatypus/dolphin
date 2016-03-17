8c4aacd74661f6c57a7ea90324ae687d34b30bbf
duckey77 <duckey77@live.com>
2016-03-17 10:38:13 -0700

    Added wii functions to DolHost

--- Dolphin-Core/DolHost.mm
+++ Dolphin-Core/DolHost.mm
@@ -26,6 +26,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "DolHost.h"
 #include "OE_OSXJoystick.h"
+#include "OE_WiimoteEmu.h"
 #include <OpenGL/gl3.h>
 #include <OpenGL/gl3ext.h>
 #import  <Cocoa/Cocoa.h>
@@ -33,28 +34,19 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "Core/BootManager.h"
 #include "Core/ConfigManager.h"
 #include "Core/Core.h"
-#include "Core/Host.h"
 #include "Core/State.h"
-#include "Core/HW/GCPadEmu.h"
-#include "Core/HW/Wiimote.h"
 #include "Core/IPC_HLE/WII_IPC_HLE_Device_usb.h"
 #include "Core/IPC_HLE/WII_IPC_HLE_WiiMote.h"
 #include "Core/PowerPC/PowerPC.h"
-#include "Common/Common.h"
-#include "Common/CommonTypes.h"
 #include "Common/CommonPaths.h"
 #include "Common/Event.h"
-#include "Common/MsgHandler.h"
 #include "Common/Logging/LogManager.h"
-#include "Common/GL/GLInterfaceBase.h"
 #include "UICommon/UICommon.h"
 #include "VideoCommon/VideoBackendBase.h"
 #include "VideoCommon/VideoConfig.h"
-#include "Videobackends/OGL/FramebufferManager.h"
-#include "InputCommon/ControllerInterface/Device.h"
+#include "InputCommon/InputConfig.h"
 #include "InputCommon/ControllerInterface/ControllerInterface.h"
-#include "InputCommon/ControllerInterface/ExpressionParser.h"
-#include "AudioCommon/AudioCommon.h"
+#include "DiscIO/VolumeCreator.h"
 
 DolHost* DolHost::m_instance = nullptr;
 static Common::Event updateMainFrameEvent;
@@ -80,26 +72,43 @@ void DolHost::Init(std::string supportDirectoryPath, std::string cpath)
     UICommon::Init();
 
     SConfig::GetInstance().bDSPHLE = true;
-    SConfig::GetInstance().m_Volume = 50;
+    SConfig::GetInstance().m_Volume = 100;
     SConfig::GetInstance().bOnScreenDisplayMessages = false;
+    SConfig::GetInstance().bMMU = true;
 
-    //Create Memorycards by GameID
+    //Get game info frome file (disc)
     GetGameInfo();
 
-    std::string _memCardPath = File::GetUserPath(D_GCUSER_IDX) + DIR_SEP + _gameRegion + DIR_SEP + _gameID;
-    std::string _memCardA = _memCardPath + "_A." + _gameRegion + ".raw";
-    std::string _memCardB = _memCardPath +  "_B." + _gameRegion + ".raw";
+    if (!_wiiGame)
+    {
+        //Create Memorycards by GameID
+        std::string _memCardPath = File::GetUserPath(D_GCUSER_IDX) + DIR_SEP + _gameRegion + DIR_SEP + _gameID;
+        std::string _memCardA = _memCardPath + "_A." + _gameRegion + ".raw";
+        std::string _memCardB = _memCardPath +  "_B." + _gameRegion + ".raw";
 
-    SConfig::GetInstance().m_strMemoryCardA = _memCardA;
-    SConfig::GetInstance().m_strMemoryCardB = _memCardB;
+        SConfig::GetInstance().m_strMemoryCardA = _memCardA;
+        SConfig::GetInstance().m_strMemoryCardB = _memCardB;
+
+        //Clear the Nand path
+        SConfig::GetInstance().m_NANDPath = "";
+    }else{
+        //clear the GC mem card paths
+        SConfig::GetInstance().m_strMemoryCardA = "";
+        SConfig::GetInstance().m_strMemoryCardB = "";
+
+        //Set the WiiNANDpath
+        SConfig::GetInstance().m_NANDPath = supportDirectoryPath  + DIR_SEP + "Wii";
+    }
 }
 
 # pragma mark - Execution
 bool DolHost::LoadFileAtPath()
 {
+    SConfig::GetInstance().bWii = _wiiGame;
+
     if(!BootManager::BootCore(_gamePath))
         return false;
-    
+
     while (!Core::IsRunning())
         updateMainFrameEvent.Wait();
 
@@ -155,6 +164,9 @@ bool DolHost::SaveState(std::string saveStateFile)
 
 bool DolHost::LoadState(std::string saveStateFile)
 {
+    if (!SConfig::GetInstance().bWii)
+        Core::SetStateFileName(saveStateFile);
+
     State::LoadAs(saveStateFile);
     return true;
 }
@@ -162,57 +174,133 @@ bool DolHost::LoadState(std::string saveStateFile)
 # pragma mark - Controls
 void DolHost::SetUpPlayerInputs()
 {
-    struct {
-        OEGCButton button;
-        std::string identifier;
-    } buttonToIdentifier[OEGCButtonCount] = {
-        { OEGCButtonUp, "OEGCButtonUp" },
-        { OEGCButtonDown, "OEGCButtonDown" },
-        { OEGCButtonLeft, "OEGCButtonLeft" },
-        { OEGCButtonRight, "OEGCButtonRight" },
-        { OEGCButtonA, "OEGCButtonA" },
-        { OEGCButtonB, "OEGCButtonB" },
-        { OEGCButtonX, "OEGCButtonX" },
-        { OEGCButtonY, "OEGCButtonY" },
-        { OEGCButtonL, "OEGCButtonL" },
-        { OEGCButtonR, "OEGCButtonR" },
-        { OEGCButtonZ, "OEGCButtonZ" },
-        { OEGCButtonStart, "OEGCButtonStart" },
-        { OEGCAnalogUp, "OEGCAnalogUp" },
-        { OEGCAnalogDown, "OEGCAnalogDown" },
-        { OEGCAnalogLeft, "OEGCAnalogLeft" },
-        { OEGCAnalogRight, "OEGCAnalogRight" },
-        { OEGCAnalogCUp, "OEGCAnalogCUp" },
-        { OEGCAnalogCDown, "OEGCAnalogCDown" },
-        { OEGCAnalogCLeft, "OEGCAnalogCLeft" },
-        { OEGCAnalogCRight, "OEGCAnalogCRight" },
-    };
+    if (SConfig::GetInstance().bWii) {
+        struct {
+            OEWiiButton button;
+            std::string identifier;
+        } buttonToIdentifier[OEWiiButtonCount] = {
+            { OEWiiMoteButtonUp, "OEWiiMoteButtonUp" },
+            { OEWiiMoteButtonDown, "OEWiiMoteButtonDown" },
+            { OEWiiMoteButtonLeft, "OEWiiMoteButtonLeft" },
+            { OEWiiMoteButtonRight, "OEWiiMoteButtonRight" },
+            { OEWiiMoteButtonA, "OEWiiMoteButtonA" },
+            { OEWiiMoteButtonB, "OEWiiMoteButtonB" },
+            { OEWiiMoteButton1, "OEWiiMoteButton1" },
+            { OEWiiMoteButton2, "OEWiiMoteButton2" },
+            { OEWiiMoteButtonPlus, "OEWiiMoteButtonPlus" },
+            { OEWiiMoteButtonMinus, "OEWiiMoteButtonMinus" },
+            { OEWiiMoteButtonHome, "OEWiiMoteButtonHome" },
+            { OEWiiNunchukAnalogUp, "OEWiiNunchukAnalogUp" },
+            { OEWiiNunchukAnalogDown, "OEWiiNunchukAnalogDown" },
+            { OEWiiNunchukAnalogLeft, "OEWiiNunchukAnalogLeft" },
+            { OEWiiNunchukAnalogRight, "OEWiiNunchukAnalogRight" },
+            { OEWiiNunchukButtonC, "OEWiiNunchukButtonC" },
+            { OEWiiNunchuckButtonZ, "OEWiiNunchukButtonZ" },
+            { OEWiiClassicButtonUp, "OEWiiClassicButtonUp" },
+            { OEWiiClassicButtonDown, "OEWiiClassicButtonDown" },
+            { OEWiiClassicButtonLeft, "OEWiiClassicButtonLeft" },
+            { OEWiiClassicButtonRight, "OEWiiClassicButtonRight" },
+            { OEWiiClassicAnalogLUp, "OEWiiClassicAnalogLUp" },
+            { OEWiiClassicAnalogLDown, "OEWiiClassicAnalogLDown" },
+            { OEWiiClassicAnalogLLeft, "OEWiiClassicAnalogLLeft" },
+            { OEWiiClassicAnalogLRight, "OEWiiClassicAnalogLRight" },
+            { OEWiiClassicAnalogRUp, "OEWiiClassicAnalogRUp" },
+            { OEWiiClassicAnalogRDown, "OEWiiClassicAnalogRDown" },
+            { OEWiiClassicAnalogRLeft, "OEWiiClassicAnalogRLeft" },
+            { OEWiiClassicAnalogRRight, "OEWiiClassicAnalogRRight" },
+            { OEWiiClassicButtonA, "OEWiiClassicButtonA" },
+            { OEWiiClassicButtonB, "OEWiiClassicButtonB" },
+            { OEWiiClassicButtonX, "OEWiiClassicButtonX" },
+            { OEWiiClassicButtonY, "OEWiiClassicButtonY" },
+            { OEWiiClassicButtonL, "OEWiiClassicButtonL" },
+            { OEWiiClassicButtonR, "OEWiiClassicButtonR" },
+            { OEWiiClassicButtonZl, "OEWiiClassicButtonZl" },
+            { OEWiiClassicButtonZr, "OEWiiClassicButtonZr" },
+            { OEWiiClassicButtonStart, "OEWiiClassicButtonStart" },
+            { OEWiiClassicButtonSelect, "OEWiiClassicButtonSelect" },
+            { OEWiiClassicButtonHome, "OEWiiClassicButtonHome" },
+        };
 
-    std::vector<ciface::Core::Device*> devices = g_controller_interface.ControllerInterface::Devices();
-    for (int player = 0; player < 4; ++player) {
-        std::string qualifier = "OE_GameDev" + std::to_string(player);
-        ciface::Core::Device* device = nullptr;
-               for (auto& d : devices) {
-                        if (d->GetName() == qualifier) {
+        std::vector<ciface::Core::Device*> devices = g_controller_interface.ControllerInterface::Devices();
+        for (int player = 0; player < 4; ++player) {
+            std::string qualifier = "OE_GameDev" + std::to_string(player);
+            ciface::Core::Device* device = nullptr;
+            for (auto& d : devices) {
+                if (d->GetName() == qualifier) {
 
-                            device = d;
-                            break;
-                        }
-               }
-        if (device == nullptr)
-                    continue;
+                    device = d;
+                    break;
+                }
+            }
+            if (device == nullptr)
+                continue;
 
-        for (int inputIndex = 0; inputIndex < OEGCButtonCount; ++inputIndex) {
-                        std::string identifier = buttonToIdentifier[inputIndex].identifier;
-                        ciface::Core::Device::Input* input = g_controller_interface.ControllerInterface::FindInput(identifier, device);
+            for (int inputIndex = 0; inputIndex < OEWiiButtonCount; ++inputIndex) {
+                std::string identifier = buttonToIdentifier[inputIndex].identifier;
+                ciface::Core::Device::Input* input = g_controller_interface.ControllerInterface::FindInput(identifier, device);
 
-                        m_playerInputs[player][buttonToIdentifier[inputIndex].button] = input;
+                m_playerInputs[player][buttonToIdentifier[inputIndex].button] = input;
+            }
+        }
+    }
+    else
+    {
+        struct {
+            OEGCButton button;
+            std::string identifier;
+        } buttonToIdentifier[OEGCButtonCount] = {
+            { OEGCButtonUp, "OEGCButtonUp" },
+            { OEGCButtonDown, "OEGCButtonDown" },
+            { OEGCButtonLeft, "OEGCButtonLeft" },
+            { OEGCButtonRight, "OEGCButtonRight" },
+            { OEGCButtonA, "OEGCButtonA" },
+            { OEGCButtonB, "OEGCButtonB" },
+            { OEGCButtonX, "OEGCButtonX" },
+            { OEGCButtonY, "OEGCButtonY" },
+            { OEGCButtonL, "OEGCButtonL" },
+            { OEGCButtonR, "OEGCButtonR" },
+            { OEGCButtonZ, "OEGCButtonZ" },
+            { OEGCButtonStart, "OEGCButtonStart" },
+            { OEGCAnalogUp, "OEGCAnalogUp" },
+            { OEGCAnalogDown, "OEGCAnalogDown" },
+            { OEGCAnalogLeft, "OEGCAnalogLeft" },
+            { OEGCAnalogRight, "OEGCAnalogRight" },
+            { OEGCAnalogCUp, "OEGCAnalogCUp" },
+            { OEGCAnalogCDown, "OEGCAnalogCDown" },
+            { OEGCAnalogCLeft, "OEGCAnalogCLeft" },
+            { OEGCAnalogCRight, "OEGCAnalogCRight" },
+        };
+
+        std::vector<ciface::Core::Device*> devices = g_controller_interface.ControllerInterface::Devices();
+        for (int player = 0; player < 4; ++player) {
+            std::string qualifier = "OE_GameDev" + std::to_string(player);
+            ciface::Core::Device* device = nullptr;
+            for (auto& d : devices) {
+                if (d->GetName() == qualifier) {
+
+                    device = d;
+                    break;
+                }
+            }
+            if (device == nullptr)
+                continue;
+
+            for (int inputIndex = 0; inputIndex < OEGCButtonCount; ++inputIndex) {
+                std::string identifier = buttonToIdentifier[inputIndex].identifier;
+                ciface::Core::Device::Input* input = g_controller_interface.ControllerInterface::FindInput(identifier, device);
+
+                m_playerInputs[player][buttonToIdentifier[inputIndex].button] = input;
+            }
         }
     }
 }
 
 void DolHost::SetButtonState(int button, int state, int player)
 {
+    if (SConfig::GetInstance().bWii && state == 1 )
+    {
+        CheckExtension(button, player);
+    }
     ciface::Core::Device::Input* input = m_playerInputs[player - 1][button];
     input->SetState(state);
 }
@@ -223,14 +311,24 @@ void DolHost::SetAxis(int button, float value, int player)
     input->SetState(value);
 }
 
+void DolHost::CheckExtension(int button, int player)
+{
+    WiimoteEmu::Wiimote* _Wiimote = ((WiimoteEmu::Wiimote*)Wiimote::GetConfig()->GetController( player - 1 ));
+
+    if( button >10 && button < 17 && _Wiimote->CurrentExtension() != 1) // Nunchuck button pressed
+        _Wiimote->SwitchExtension(1); // Set Nunchuk as extenstion
+    else if (button > 16 && _Wiimote->CurrentExtension() != 2)  // Classic Controller button pressed
+        _Wiimote->SwitchExtension(2); // Set Classic Controller as extension
+}
 # pragma mark - DVD info
 void DolHost::GetGameInfo()
 {
-    std::unique_ptr<DiscIO::IVolume> pVolume(DiscIO::CreateVolumeFromFilename(_gamePath));
+    std::unique_ptr<DiscIO::IVolume> pVolume(DiscIO::CreateVolumeFromFilename( _gamePath ));
 
-    _gameID = pVolume->GetUniqueID();
-    _gameRegion =GetRegionOfCountry(pVolume->GetCountry());
-    _gameName = pVolume->GetInternalName();
+    _gameID = pVolume -> GetUniqueID();
+    _gameRegion = GetRegionOfCountry(pVolume -> GetCountry());
+    _gameName = pVolume -> GetInternalName();
+    _wiiGame = pVolume->GetVolumeType() == DiscIO::IVolume::WII_DISC;
 }
 
 std::string DolHost::GetRegionOfCountry(int country)
@@ -255,7 +353,7 @@ std::string DolHost::GetRegionOfCountry(int country)
         case DiscIO::IVolume::COUNTRY_SPAIN:
         case DiscIO::IVolume::COUNTRY_WORLD:
             return EUR_DIR;
-            
+
         case DiscIO::IVolume::COUNTRY_UNKNOWN:
         default:
             return nullptr;
@@ -280,8 +378,7 @@ void Host_SetStartupDebuggingParameters()
     NSLog(@"DolphinCore: Set Startup Debugging Parameters");
     SConfig& StartUp = SConfig::GetInstance();
     StartUp.bEnableDebugging = false;
-    StartUp.bBootToPause = false;
-    StartUp.bWii = false;
+    StartUp.bBootToPause = true;
 }
 
 void Host_NotifyMapLoaded() {}
