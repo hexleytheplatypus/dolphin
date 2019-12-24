1d56658df6a5e1e630fd5375528a5b8af46874a2
Duckey77 <duckey77@live.com>
2019-12-24 13:56:25 -0700

    Stub The Config Layer with Compatibility Fie

---
+++ Compatibility/Common/Layer.cpp
@@ -0,0 +1,100 @@
+// Copyright 2016 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#include <algorithm>
+#include <cstring>
+#include <map>
+
+#include "Common/Config/Config.h"
+#include "Common/Config/Layer.h"
+
+namespace Config
+{
+ConfigLayerLoader::ConfigLayerLoader(LayerType layer) : m_layer(layer)
+{
+}
+
+ConfigLayerLoader::~ConfigLayerLoader() = default;
+
+LayerType ConfigLayerLoader::GetLayer() const
+{
+  return m_layer;
+}
+
+Layer::Layer(LayerType type) : m_layer(type)
+{
+}
+
+Layer::Layer(std::unique_ptr<ConfigLayerLoader> loader)
+    : m_layer(loader->GetLayer()), m_loader(std::move(loader))
+{
+  Load();
+}
+
+Layer::~Layer()
+{
+  Save();
+}
+
+bool Layer::Exists(const ConfigLocation& location) const
+{
+  const auto iter = m_map.find(location);
+  return iter != m_map.end() && iter->second.has_value();
+}
+
+bool Layer::DeleteKey(const ConfigLocation& location)
+{
+  m_is_dirty = true;
+  bool had_value = false;
+  const auto iter = m_map.find(location);
+  if (iter != m_map.end() && iter->second.has_value())
+  {
+    iter->second.reset();
+    had_value = true;
+  }
+
+  return had_value;
+}
+
+void Layer::DeleteAllKeys()
+{
+  m_is_dirty = true;
+  for (auto& pair : m_map)
+  {
+    pair.second.reset();
+  }
+}
+
+Section Layer::GetSection(System system, const std::string& section)
+{
+  return Section{m_map.lower_bound(ConfigLocation{system, section, ""}),
+                 m_map.lower_bound(ConfigLocation{system, section + '\001', ""})};
+}
+
+ConstSection Layer::GetSection(System system, const std::string& section) const
+{
+  return ConstSection{m_map.lower_bound(ConfigLocation{system, section, ""}),
+                      m_map.lower_bound(ConfigLocation{system, section + '\001', ""})};
+}
+
+void Layer::Load()
+{
+    return;
+}
+
+void Layer::Save()
+{
+    return;
+}
+
+LayerType Layer::GetLayer() const
+{
+  return m_layer;
+}
+
+const LayerMap& Layer::GetLayerMap() const
+{
+  return m_map;
+}
+}  // namespace Config
