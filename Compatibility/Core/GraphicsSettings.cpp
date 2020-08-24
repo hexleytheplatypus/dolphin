44eb2510c738b2ff1347dba528b799beecf09e51
Duckey77 <duckey77@live.com>
2020-08-24 13:48:01 -0700

    Disable Ubershaders they are slower and stutter more

--- Compatibility/Core/GraphicsSettings.cpp
+++ Compatibility/Core/GraphicsSettings.cpp
@@ -84,12 +84,12 @@ const Info<int> GFX_COMMAND_BUFFER_EXECUTE_INTERVAL{
     {System::GFX, "Settings", "CommandBufferExecuteInterval"}, 100};
 #endif
 
-////OpenEmu change ShaderCache True, WaitForShaders ture, Compiler and Precompiler thread -1
+////OpenEmu change ShaderCache True, WaitForShaders false, Compiler and Precompiler thread -1
 const Info<bool> GFX_SHADER_CACHE{{System::GFX, "Settings", "ShaderCache"}, true};
 const Info<bool> GFX_WAIT_FOR_SHADERS_BEFORE_STARTING{
     {System::GFX, "Settings", "WaitForShadersBeforeStarting"}, false};
 const Info<ShaderCompilationMode> GFX_SHADER_COMPILATION_MODE{
-    {System::GFX, "Settings", "ShaderCompilationMode"}, ShaderCompilationMode::AsynchronousUberShaders};
+    {System::GFX, "Settings", "ShaderCompilationMode"}, ShaderCompilationMode::Synchronous};
 const Info<int> GFX_SHADER_COMPILER_THREADS{{System::GFX, "Settings", "ShaderCompilerThreads"}, -1};
 const Info<int> GFX_SHADER_PRECOMPILER_THREADS{
     {System::GFX, "Settings", "ShaderPrecompilerThreads"}, -1};
